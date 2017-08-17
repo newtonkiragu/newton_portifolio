cb00f06aae32741b911a9c15646b6b7f4b7b3d1f
newtonkiragu <karanunewton4@gmail.com>
2017-08-17 12:13:32 +0300

    added the functions for scrolspy

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,5 +1,21 @@
+$(document).ready(function() {
   $(".button-collapse").sideNav();
   $('.parallax').parallax();
   $('.scrollspy').scrollSpy().aciveClass();
 
 
+  function(id) {
+    return 'a[href="#' + id + '"]';
+  }
+
+  $(window).scroll(function() {
+    var scrollPos = $(window).scrollTop(),
+      navbar = $('.navbar');
+
+    if (scrollPos > 200) {
+      navbar.addClass('alt-color');
+    } else {
+      navbar.removeClass('alt-color');
+    }
+  });
+});
